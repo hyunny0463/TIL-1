ce21b15db284621d527c0410b22e9e6a2527d775
Justin <edujustin.hphk@gmail.com>
2019-10-02 15:57:57 +0900

    191002 | Create, admin finish

--- 03_Django/04_django_crud_review/articles/views.py
+++ 03_Django/04_django_crud_review/articles/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import Article
 
 def index(request):
-    return render(request, 'articles/index.html')
+    articles = Article.objects.all()
+    # print(articles)
+    # print(type(articles))
+    context = {'articles': articles}
+    return render(request, 'articles/index.html', context)
 
 def new(request):
     return render(request, 'articles/new.html')
@@ -11,8 +15,18 @@ def create(request):
     title = request.GET.get('title')
     content = request.GET.get('content')
 
-    #1. 
-    article = Article()
-    article.title = title
-    article.content = content
-    article.save()
+    #1. 첫번째 방법
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+
+    #2. 두번째 방법
+    # article = Article(title=title, content=content)
+    # article.save()
+
+    #3. 세번째 방법
+    Article.objects.create(title=title, content=content)
+
+    return render(request, 'articles/create.html')
+
