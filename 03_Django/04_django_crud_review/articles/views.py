f02880fbcd2bae1658aff0b44548366fcb1eb905
Justin <edujustin.hphk@gmail.com>
2019-10-04 17:12:13 +0900

    191004 | app name, path name

--- 03_Django/04_django_crud_review/articles/views.py
+++ 03_Django/04_django_crud_review/articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
 from .models import Article
 
 def index(request):
@@ -13,6 +14,17 @@ def new(request):
     return render(request, 'articles/new.html')
 
 def create(request):
+    try:
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Article(title=title, content=content)
+        article.full_clean()
+    except ValidationError:
+        raise ValidationError('Your Error Message')
+    else:
+        article.save()
+        return redirect('articles:detail', article.pk)
+    """
     title = request.POST.get('title')
     # print(title)
     content = request.POST.get('content')
@@ -31,9 +43,25 @@ def create(request):
     # Article.objects.create(title=title, content=content)
 
     return redirect(f'/articles/{article.pk}/')
-
+    """
 def detail(request, pk):
     article = Article.objects.get(pk=pk)
     context = {'article': article}
     return render(request, 'articles/detail.html', context)
 
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {'article': article}
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
